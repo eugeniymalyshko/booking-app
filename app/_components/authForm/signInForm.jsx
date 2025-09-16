008de02d845af5c775a0e1af3183319b4638d2b5
irzi <genikm@ukr.net>
2025-09-16 20:50:26 +0300

    work with auth

--- app/_components/authForm/signInForm.jsx
+++ app/_components/authForm/signInForm.jsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { useState } from "react";
+import { signIn } from "next-auth/react";
 import Button from "../Button/Button";
 
 export default function signInForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  function formSubmit(e) {
+  async function formSubmit(e) {
     e.preventDefault();
-    console.log("submit");
+    const singInData = await signIn("credentials", {
+      username: username,
+      password: password,
+    });
+    console.log(singInData);
   }
   return (
     <form onSubmit={formSubmit}>
--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,31 +1,46 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/app/lib/prisma";
+import { compare } from "bcrypt";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        username: { label: "Username", type: "text", placeholder: "Логін" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
           return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
+
+        const existingUser = await prisma.user.findUnique({
+          where: { username: credentials?.username },
+        });
+
+        if (!existingUser) {
+          return null;
+        }
+
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password
+        );
+
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return { id: `${existingUser.id}`, username: existingUser.username };
       },
     }),
   ],
